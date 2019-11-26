5e185220443645cc69f5c13258882de5fa18fbdd
halabito8 <halabito8@gmail.com>
2019-11-26 12:56:16 -0600

    no conecta el js

---
+++ js/registrar.js
@@ -0,0 +1,13 @@
+$("#Registrarse").click(function(e) {
+  //e.preventDefault();
+    //validación de campos y luego ajax
+    alert("Hola");
+  // var ExpCadena = /^[a-zñÑáéíóúÁÉÍÓÚ]+(\s[a-zñÑáéíóúÁÉÍÓÚ]+)*$/i;
+  //
+  // if ( !ExpCadena.test( $("#nombre").val() ) ) {
+  //  $("#nombre").css("background-color", "red");
+  // }
+  // else{
+  //   $("#nombre").css("background-color", "green");
+  // }
+});
