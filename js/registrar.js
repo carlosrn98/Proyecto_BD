1e1561b86cb61830062257ff93857c2124480bf1
halabito8 <halabito8@gmail.com>
2019-11-28 14:14:07 -0600

    manda alert falta conectar a php

--- js/registrar.js
+++ js/registrar.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
   var flag=0;
- $("#Registrarse").click(function (event){
+  var ExpCadena = /^[a-zñÑáéíóúÁÉÍÓÚ]+(\s[a-zñÑáéíóúÁÉÍÓÚ]+)*$/i;
+ $("#Registrarse").click(function (e){
+   alertas="";
+   e.preventDefault();
+   if ( !ExpCadena.test( $("#nombre").val() ) )
+     alertas="Ningun campo puede tener ningun caracter especial";
+   if ( !ExpCadena.test( $("#apellido").val() ) )
+     alertas="Ningun campo puede tener ningun caracter especial";
+   if ( !ExpCadena.test( $("#usuario").val() ) )
+     alertas="Ningun campo puede tener ningun caracter especial";
+   if ( !ExpCadena.test( $("#password").val() ) )
+     alertas="Ningun campo puede tener ningun caracter especial";
+   if ( !ExpCadena.test( $("#confirm_password").val() ) )
+     alertas="Ningun campo puede tener ningun caracter especial";
+    if(alertas)
+      alert(alertas);
+    else {
+      alert("Eso compa");
+    }
+  });
 
- });
+
 
  $('#password, #confirm_password').on('keyup', function () {
    if ($('#password').val() == $('#confirm_password').val()){
      $('#message').html('Coinciden').css('color', 'green');
-     flag=1;
+     flag+=1;
    }
    else{
      $('#message').html('No coinciden').css('color', 'red');
-     flag=0;
    }
-    console.log(flag);
+
  });
 });
