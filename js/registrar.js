cd8f8a79de7f01dc031039a742f5468abf32c074
halabito8 <halabito8@gmail.com>
2019-11-29 13:58:24 -0600

    ya hace todas las validaciones

--- js/registrar.js
+++ js/registrar.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   var flag=0;
   var ExpCadena = /^[a-zñÑáéíóúÁÉÍÓÚ]+(\s[a-zñÑáéíóúÁÉÍÓÚ]+)*$/i;
+  var regexemail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
  $("#Registrarse").click(function (e){
    alertas="";
    e.preventDefault();
@@ -16,24 +17,65 @@ $(document).ready(function(){
      alertas="Ningun campo puede tener ningun caracter especial o estar vacio";
      $('#usuario').css('background-color', 'red');
    }
-   if ( !ExpCadena.test( $("#password").val() ) ){
-     alertas="Ningun campo puede tener ningun caracter especial o estar vacio";
+   if($("input[name='genero']:checked").length <=0){
+     alertas+="\nNo ha seleccionado genero";
+     $('#genero').css('background-color', 'red');
+   }
+   if(!regexemail.test( $("#NuevoCorreo").val() )){
+     alertas+="\nFavor de meter un correo con formato xx@xx.xx";
+     $('#NuevoCorreo').css('background-color', 'red');
+   }
+   if ( ( $("#password").val() ) != $('#confirm_password').val()){
+     alertas+="\nLas contrasenas tienen que coincidir";
      $('#password').css('background-color', 'red');
    }
-   if ( !ExpCadena.test( $("#confirm_password").val() ) ){
-     alertas="Ningun campo puede tener ningun caracter especial o estar vacio";
-     $('#confirm_password').css('background-color', 'red');
-   }
-    if(alertas && ($('#password').val() != $('#confirm_password').val()))
+    if(alertas)
       alert(alertas);
     else {
-      alert("Eso compa");
+      var date = new Date($('#FN').val());
+      day = date.getDate() + 1;
+      month = date.getMonth() + 1;
+      year = date.getFullYear();
+      fecha=year+"-"+month+"-"+day;
+
+      inpgenero=$("input[name='genero']:checked").val();
+
+      var params="NuevoNmbr="+$("#nombre").val()+"&NuevoApe="+$("#apellido").val()+"&genero="+inpgenero+"&FN="+fecha;
+      params+="&NuevoCorreo="+$("#NuevoCorreo").val()+"&NuevoUsr="+$("#usuario").val();
+      params+="&passwrd1="+$("#password").val();
+
+      url='./registrarusuario.php';
+
+      $.ajax({
+        url: url,
+        dataType: 'html',
+        type: 'POST',
+        async: true,
+        data: params,
+        success:muestraContenido,
+        error: funcionErrors
+      });
     }
   });
 
+  function muestraContenido(result, status, xhr){
+    console.log(result);
+    window.location.assign("./index.php");
+  }
 
+  function funcionErrors(xhr, status, error){
+    alert(xhr);
+  }
 
- $('#password, #confirm_password').on('keyup', function () {
+  $('#NuevoCorreo').on('keyup',function (){
+    if(regexemail.test( $("#NuevoCorreo").val() )){
+      $('#message2').html('Si es un email').css('color', 'green');
+    }
+    else
+      $('#message2').html('NO es un email').css('color', 'red');
+  });
+
+  $('#password, #confirm_password').on('keyup', function () {
    if ($('#password').val() == $('#confirm_password').val()){
      $('#message').html('Coinciden').css('color', 'green');
      flag+=1;
@@ -42,5 +84,5 @@ $(document).ready(function(){
      $('#message').html('No coinciden').css('color', 'red');
    }
 
- });
+  });
 });
